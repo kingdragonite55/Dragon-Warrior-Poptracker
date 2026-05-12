875c16045626609022e11351a2f7572506880bca
kingdragonite55 <eastereggalt@gmail.com>
2026-05-12 02:28:09 -0400

    Update logic.lua

--- scripts/logic/logic.lua
+++ scripts/logic/logic.lua
@@ -10,51 +10,19 @@ end
 
 -- === Key Items ===
 
-function fairy_flute()
-    return Tracker:ProviderCountForCode("fairy_flute") > 0
-end
-
-function rainbow_drop()
-    return Tracker:ProviderCountForCode("rainbow_drop") > 0
-end
-
-function gwaelins_love()
-    return Tracker:ProviderCountForCode("gwaelins_love") > 0
-end
-
-function erdricks_tablet()
-    return Tracker:ProviderCountForCode("erdricks_tablet") > 0
-end
-
 hasprincess = function()
     return is_location_checked("@Main/Swamp Cave/Rescue Princess Gwaelin")
        and not is_location_checked("@Main/Tantagel Castle/Returned Gwaelin to Castle")
 end
 
-function ball_of_light()
-    return Tracker:ProviderCountForCode("ball_of_light") > 0
-end
-
-function erdricks_token()
-  return has("erdricks_token") or has("erdricks_token_checked")
-end
-
-function stones_of_sunlight()
-  return has("stones_of_sunlight") or has("stones_of_sunlight_checked")
-end
-
-function silver_harp()
-  local obj = Tracker:FindObjectForCode("silver_harp")
-  return obj and obj.CurrentStage > 0
-end
-
-
-function staff_of_rain()
-  return has("staff_of_rain") or has("staff_of_rain_checked")
-end
-
 function rainbow_shrine()
-  return staff_of_rain() and stones_of_sunlight() and erdricks_token()
+	if has("staff_of_rain") and has("stones_of_sunlight") and has("magic_key") then
+		if has("searchsanity") then
+			return has("erdricks_token" and has("fairy_flute")
+		else
+			return true
+		end
+	end
 end
 
 
